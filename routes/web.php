90eea1d0af1c7bc43abf045c6cfe767322f1e37d
mwagenipecy <percyegno@gmail.com>
2025-01-28 20:14:04 +0300

    frontend ui

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('test',function(){
+ return view('demo');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
