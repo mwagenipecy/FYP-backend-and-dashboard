a8c5e3875dd85b88ef39e685a6389a39609cfbc2
mwagenipecy <percyegno@gmail.com>
2025-04-21 22:19:50 +0300

    updated

---
+++ app/Livewire/Project/Phase/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Project\Phase;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.project.phase.create');
+    }
+}
